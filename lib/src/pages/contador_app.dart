c6d81f27157d8f52a6d1fd87cbb69013beb43250
ddienu <rockdann@hotmail.com>
2022-01-29 01:48:55 -0500

    Add: Adding two new buttons.
    Adding two new buttons to the app, one that sustracting and another one to reset the number of clicks.

--- lib/src/pages/contador_app.dart
+++ lib/src/pages/contador_app.dart
@@ -31,24 +31,53 @@ int _conteo = 0;
           ],
 ),
       ),
-      floatingActionButton: FloatingActionButton (
-        child: Icon (Icons.add),
-        
-        onPressed: () {
-
-          print ('Hola Mundo!');
-
-         setState(() {
-
-         _conteo++;    
-
-          });
-        },
-
-
-
-      )
+      floatingActionButton: _crearBotones()
     );
+  }
+   void _agregar(){
+
+          setState(() => _conteo++);
+
+        }
+
+        void _sustraer (){
+
+          setState(() => _conteo--);
+        }
+
+        void _reset (){
+
+          setState(() => _conteo = 0);
+
+        }
+  Widget _crearBotones (){
+
+      return Row(
+
+        mainAxisAlignment: MainAxisAlignment.end,
+
+        children: <Widget>[
+
+        SizedBox(width: 30.0),
+        FloatingActionButton (child: Icon (Icons.exposure_zero), onPressed: _reset),
+        Expanded (child: SizedBox (width: 5.0)),
+        FloatingActionButton (child: Icon (Icons.remove), onPressed: _sustraer),
+        SizedBox (width: 5.0),
+        FloatingActionButton (child: Icon (Icons.add), onPressed: _agregar),
+
+        ]
+        );
+
+     
+
+
+      
+    
+
+      
+
+
+    
   }
 
 } 
